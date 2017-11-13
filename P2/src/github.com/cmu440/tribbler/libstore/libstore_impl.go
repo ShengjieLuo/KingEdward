9945d077ecee9b1da7867327af9383761871c004
ShengjieLuo <shengjil@andrew.cmu.edu>
2017-11-13 06:51:36 +0000

    Basic implementation

--- P2/src/github.com/cmu440/tribbler/libstore/libstore_impl.go
+++ P2/src/github.com/cmu440/tribbler/libstore/libstore_impl.go
@@ -1,75 +1,219 @@
 package libstore
 
 import (
+	"errors"
 	"fmt"
 	"net/rpc"
-	"errors"
+	"time"
 
 	"github.com/cmu440/tribbler/rpc/storagerpc"
 	//"github.com/cmu440/tribbler/rpc/librpc"
 	//"github.com/cmu440/tribbler/storageserver"
 )
 
-type libstore struct {
-	HostPort string
-	client *rpc.Client
+/*Status=0: insert string
+  Status=1: insert []string
+*/
+type cacheArgs struct {
+    Status	int
+	Key		string
+	ReplyA	storagerpc.GetReply
+	ReplyB  storagerpc.GetListReply
 }
 
-// NewLibstore creates a new instance of a TribServer's libstore. masterServerHostPort
-// is the master storage server's host:port. myHostPort is this Libstore's host:port
-// (i.e. the callback address that the storage servers should use to send back
-// notifications when leases are revoked).
-//
-// The mode argument is a debugging flag that determines how the Libstore should
-// request/handle leases. If mode is Never, then the Libstore should never request
-// leases from the storage server (i.e. the GetArgs.WantLease field should always
-// be set to false). If mode is Always, then the Libstore should always request
-// leases from the storage server (i.e. the GetArgs.WantLease field should always
-// be set to true). If mode is Normal, then the Libstore should make its own
-// decisions on whether or not a lease should be requested from the storage server,
-// based on the requirements specified in the project PDF handout.  Note that the
-// value of the mode flag may also determine whether or not the Libstore should
-// register to receive RPCs from the storage servers.
-//
-// To register the Libstore to receive RPCs from the storage servers, the following
-// line of code should suffice:
-//
-//     rpc.RegisterName("LeaseCallbacks", librpc.Wrap(libstore))
-//
-// Note that unlike in the NewTribServer and NewStorageServer functions, there is no
-// need to create a brand new HTTP handler to serve the requests (the Libstore may
-// simply reuse the TribServer's HTTP handler since the two run in the same process).
-func NewLibstore(masterServerHostPort, myHostPort string, mode LeaseMode) (Libstore, error) {
-	cli,err := rpc.DialHTTP("tcp",masterServerHostPort)
-	if err!=nil{
-		return nil,err
-	}
+/*Status=1: Not in Cache && Want Lease
+  Status=2: Not in Cache && Donot want Lease
+  Status=3: In Cache && return string
+  Status=4: In Cache && return string list
+*/
+type cacheReply struct {
+	Status	int
+	ValueA	string
+	ValueB	[]string
+}
 
-	return &libstore{client: cli, HostPort:myHostPort}, nil
+
+type libstore struct {
+	HostPort   string
+	client     *rpc.Client
+	Lease      LeaseMode
+	CacheA     map[string]string
+	CacheB     map[string][]string
+	//Get()/Getlist() asks Main Routine to find if they want lease
+	QueryReqs  chan string
+	QueryReps  chan cacheReply
+	//Get() insert items into CacheA, GetList() insert items into CacheB
+	Insert     chan cacheArgs
+	RemoveReqs chan string
+	RemoveReps chan storagerpc.Status
+	Ticker     chan int
+}
+
+func timeRoutine(ls *libstore) {
+    i := 1
+    ticker := time.NewTicker(time.Second)
+    for _ = range ticker.C {
+		ls.Ticker <- i
+		i += 1
+    }
+}
+
+func mainRoutine(ls *libstore) {
+	queryList  := make([][]string,storagerpc.QueryCacheSeconds)
+	expireMap  := make(map[string]int)
+	timeTicker := 0
+	queryUnit  := make([]string)
+	for {
+		select{
+			case timeTicker:<-ls.Ticker:
+				//0. Remove outdate queryunit
+				queryList = append(queryList[:0], queryList[1:]...)
+				//1. Add new queryunit
+				queryList = append(queryList,queryUnit)
+				queryUnit = queryUnit[:0]
+				//2. Expire Items
+				for key,value := range expireMap {
+					if value==timeTicker {
+						delete(expireMap,key)
+						_,ok1 := ls.CacheA[key];
+						_,ok2 := ls.CacheB[key];
+						if ok1 {
+							delete(ls.CacheA,key);
+						} else if ok2 {
+							delete(ls.CacheB,key);
+						}
+					}
+				}
+			case req:<-ls.QueryReqs:
+				//0. Find the key in CacheA
+				value,ok := ls.CacheA[req]
+				if ok{
+					ls.QueryReps <- cacheReply{Status:3,ValueA:value,ValueB:nil}
+					continue
+				}
+				//1. Find the key in CacheB
+				value,ok := ls.CacheB[req]
+				if ok{
+					ls.QueryReps <- cacheReply{Status:4,ValueA:nil,ValueB:value}
+					continue
+				}
+				//2. Find the key required lease
+				count := 0
+				for _,queryUnit := range(queryList){
+					for _,query := range(queryUnit) {
+						if query==req {
+							count += 1
+							if count>=storage {
+								break;
+							}
+						}
+					}
+				}
+				queryUnit = append(queryUnit,req)
+				if count>=storage {
+					ls.QueryReps <- cacheReply{Status:1,ValueA:nil,ValueB:nil}
+				} else {
+					ls.QueryReps <- cacheReply{Status:2,ValueA:nil,ValueB:nil}
+				}
+			case arg:<-ls.Insert:
+				//0. Insert into cache
+				if arg.Status==0{
+					ls.CacheA[arg.Key] = arg.ValueA
+				} else if arg.Status==1 {
+					ls.CacheB[arg.Key] = arg.ValueB
+				}
+				//1. Update ExpireMap
+				expireMap[arg.Key] = timeTicker + arg.Lease.LeaseSeconds + arg.Lease.LeaseGuardSeconds
+			case rem:<-ls.RemoveReqs:
+				//0. Remove from Cache
+				_,ok1 := ls.CacheA[rem];
+				_,ok2 := ls.CacheB[rem];
+				if ok1 {
+					delete(ls.CacheA,rem);
+					ls.RemoveReps<-storagerpc.OK
+				} else if ok2 {
+					delete(ls.CacheB,rem);
+					ls.RemoveReps<-storagerpc.OK
+				} else {
+					ls.RemoveReps<-storagerpc.KeyNotFound
+				}
+				//1. Update Expiremap
+				delete(expireMap,arg.Key)
+		}
+	}
+}
+
+func NewLibstore(masterServerHostPort, myHostPort string, mode LeaseMode) (Libstore, error) {
+	cli, err := rpc.DialHTTP("tcp", masterServerHostPort)
+	if err != nil {
+		return nil, err
+	}
+	cacheA    := make(map[string]string)
+	cacheB    := make(map[string][]string)
+	queryReqs := make(chan string)
+	queryReps := make(chan cacheReply)
+	insert    := make(chan cahceArgs)
+	ticker    := make(chan int)
+	removeReqs:= make(chan string)
+	removeReps:= make(chan storagerpc.Status)
+	ls        := &libstore{client: cli, HostPort: myHostPort, Lease:mode, CacheA:cacheA, CacheB:cacheB,
+		      QueryReqs: queryReqs, QueryReps: queryReps, InsertA: insertA, InsertB: insertB,
+              RemoveReqs:removeReqs, RemoveReps:removeReps,Ticker:ticker}
+	go timeRoutine(ls)
+	go mainRoutine(ls)
+	return ls,nil
 }
 
 func (ls *libstore) Get(key string) (string, error) {
-	//fmt.Printf("[libstore] Get(%s)\n",key)
-	args := &storagerpc.GetArgs{key,false,ls.HostPort}
+	fmt.Printf("[libstore] Get(%s)\n",key)
+
+    //Step0: Quer Cache
+	var leaseFlag bool
+	ls.QueryReqs <- key
+	rep :<-ls.QuerReps
+	if (rep.Status==3){
+		return rep.ValueA,nil
+	} else if (rep.Status==4){
+		return nil,errors.New("Found incompatiable type in cache")
+	} else if (rep.Status==1){
+		leaseFlag = true
+	} else {
+		leaseFlag = false
+	}
+
+	//Step1:Prepare data and configuration
+	if ls.Lease == Always {
+		leaseFlag = true
+	} else if ls.Lease == Never {
+		leaseFlag = false
+	}
+	args := &storagerpc.GetArgs{key, leaseFlag, ls.HostPort}
+
+	//Step2: Send RPC request
 	var reply storagerpc.GetReply
-	err := ls.client.Call("StorageServer.Get",args,&reply)
-	if err!=nil{
+	err := ls.client.Call("StorageServer.Get", args, &reply)
+	if err != nil {
 		fmt.Println(err)
-		return "",err
+		return "", err
+	}
+
+	//Step3:Fetch reply
+	if leaseFlag && reply.Lease.Granted {
+		insert <-cacheArgs{Status:0,Key:key,ReplyA:reply,ReplyB:nil}
 	}
 	if reply.Status == storagerpc.OK {
-		return reply.Value,nil
+		return reply.Value, nil
 	} else {
-		return reply.Value,errors.New(string(reply.Status))
+		return reply.Value, errors.New(string(reply.Status))
 	}
 }
 
 func (ls *libstore) Put(key, value string) error {
-	//fmt.Printf("[libstore] Put(%s->%s)\n",key,value)
-	args := &storagerpc.PutArgs{key,value}
+	fmt.Printf("[libstore] Put(%s->%s)\n",key,value)
+	args := &storagerpc.PutArgs{key, value}
 	var reply storagerpc.PutReply
-	err := ls.client.Call("StorageServer.Put",args,&reply)
-	if err!=nil{
+	err := ls.client.Call("StorageServer.Put", args, &reply)
+	if err != nil {
 		fmt.Println(err)
 		return err
 	}
@@ -84,12 +228,12 @@ func (ls *libstore) Delete(key string) error {
 	//fmt.Printf("[libstore] Delete(%s)\n",key)
 	args := &storagerpc.DeleteArgs{key}
 	var reply storagerpc.DeleteReply
-	err := ls.client.Call("StorageServer.Delete",args,&reply)
-	if err!=nil {
+	err := ls.client.Call("StorageServer.Delete", args, &reply)
+	if err != nil {
 		fmt.Println(err)
 		return err
 	}
-	if reply.Status == storagerpc.OK{
+	if reply.Status == storagerpc.OK {
 		return nil
 	} else {
 		return errors.New(string(reply.Status))
@@ -97,31 +241,58 @@ func (ls *libstore) Delete(key string) error {
 }
 
 func (ls *libstore) GetList(key string) ([]string, error) {
-	//fmt.Printf("[libstore] GetList(%s)\n",key)
-	args := &storagerpc.GetArgs{key,false,ls.HostPort}
-	var reply storagerpc.GetListReply
-	err := ls.client.Call("StorageServer.GetList",args,&reply)
-	if err!=nil {
-		fmt.Println(err)
-		return nil,err
-	}
-	if reply.Status == storagerpc.OK{
-		return reply.Value,nil
+	fmt.Printf("[libstore] GetList(%s)\n",key)
+	//Step0: Quer Cache
+	var leaseFlag bool
+	var rep cacheReply
+	ls.QueryReqs <- key
+	rep <-ls.QuerReps
+	if (rep.Status==4){
+		return rep.ValueB,nil
+	} else if (rep.Status==3){
+		return nil,errors.New("Found incompatiable type in cache")
+	} else if (rep.Status==1){
+		leaseFlag = true
 	} else {
-		return reply.Value,errors.New(string(reply.Status))
+		leaseFlag = false
+	}
+
+	//Step1: Prepare data and configuration
+	if ls.Lease == Always {
+		leaseFlag = true
+	} else if ls.Lease == Never {
+		leaseFlag = false
+	}
+	args := &storagerpc.GetArgs{key, leaseFlag, ls.HostPort}
+
+	//Step2:Send rpc request
+	var reply storagerpc.GetListReply
+	err := ls.client.Call("StorageServer.GetList", args, &reply)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	//Step2:Fetch reply
+	if leaseFlag && reply.Lease.Granted {
+		insert <-cacheArgs{Status:1,Key:key,ReplyA:nil,ReplyB:reply}
+	}
+	if reply.Status == storagerpc.OK {
+		return reply.Value, nil
+	} else {
+		return reply.Value, errors.New(string(reply.Status))
 	}
 }
 
 func (ls *libstore) RemoveFromList(key, removeItem string) error {
 	//fmt.Printf("[libstore] RemoveFromList(%s->%s)\n",key,removeItem)
-	args := &storagerpc.PutArgs{key,removeItem}
+	args := &storagerpc.PutArgs{key, removeItem}
 	var reply storagerpc.PutReply
-	err := ls.client.Call("StorageServer.RemoveFromList",args,&reply)
-	if err!=nil {
+	err := ls.client.Call("StorageServer.RemoveFromList", args, &reply)
+	if err != nil {
 		fmt.Println(err)
 		return err
 	}
-	if reply.Status == storagerpc.OK{
+	if reply.Status == storagerpc.OK {
 		return nil
 	} else {
 		return errors.New(string(reply.Status))
@@ -130,14 +301,14 @@ func (ls *libstore) RemoveFromList(key, removeItem string) error {
 
 func (ls *libstore) AppendToList(key, newItem string) error {
 	//fmt.Printf("[libstore] AppendToList(%s->%s)\n",key,newItem)
-	args := &storagerpc.PutArgs{key,newItem}
+	args := &storagerpc.PutArgs{key, newItem}
 	var reply storagerpc.PutReply
-	err := ls.client.Call("StorageServer.AppendToList",args,&reply)
-	if err!=nil {
+	err := ls.client.Call("StorageServer.AppendToList", args, &reply)
+	if err != nil {
 		fmt.Println(err)
 		return err
 	}
-	if reply.Status == storagerpc.OK{
+	if reply.Status == storagerpc.OK {
 		return nil
 	} else {
 		return errors.New(string(reply.Status))
@@ -145,5 +316,7 @@ func (ls *libstore) AppendToList(key, newItem string) error {
 }
 
 func (ls *libstore) RevokeLease(args *storagerpc.RevokeLeaseArgs, reply *storagerpc.RevokeLeaseReply) error {
-	return errors.New("not implemented")
+	ls.RemoveReqs <- args.Key
+	reply.Status  <- ls.RemoveReps
+	return nil
 }
--- P2/src/github.com/cmu440/tribbler/tribserver/tribserver_impl.go
+++ P2/src/github.com/cmu440/tribbler/tribserver/tribserver_impl.go
@@ -6,18 +6,18 @@ import (
 	//"errors"
 	"fmt"
 	"net/http"
+	"sort"
 	"strconv"
 	"time"
-	"sort"
 	//"strings"
 
-	"github.com/cmu440/tribbler/rpc/tribrpc"
 	"github.com/cmu440/tribbler/libstore"
+	"github.com/cmu440/tribbler/rpc/tribrpc"
 )
 
 type tribServer struct {
 	//client *rpc.Client //Client in the tribServer<->backendStorage
-	lib	libstore.Libstore
+	lib libstore.Libstore
 }
 
 // NewTribServer creates, starts and returns a new TribServer. masterServerHostPort
@@ -30,43 +30,48 @@ func NewTribServer(masterServerHostPort, myHostPort string) (TribServer, error) 
 	tribServer := new(tribServer)
 
 	//Step1:Initialize libstore
-	lib,errlib := libstore.NewLibstore(masterServerHostPort, myHostPort,libstore.Never)
+	lib, errlib := libstore.NewLibstore(masterServerHostPort, myHostPort, libstore.Never)
 	if errlib != nil {
 		//fmt.Println(errlib)
-		return nil,errlib
+		return nil, errlib
 	}
 	tribServer.lib = lib
 
 	//Step2: Establish the http listener to listen tribclient
-	listener, err2 := net.Listen("tcp",myHostPort)
-	if err2 != nil{
-		return nil,err2
+	listener, err2 := net.Listen("tcp", myHostPort)
+	if err2 != nil {
+		return nil, err2
 	}
-	err := rpc.RegisterName("TribServer",tribrpc.Wrap(tribServer))
+	err := rpc.RegisterName("TribServer", tribrpc.Wrap(tribServer))
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
+	err =  rpc.RegisterName("LeaseCallbacks",librpc.Wrap(libstore))
+	if err != nil {
+		return nil, err
+	}
+
 	rpc.HandleHTTP()
-	go http.Serve(listener,nil)
+	go http.Serve(listener, nil)
 
 	//Step3: Initalize the key-value store
-	tribServer.lib.Put("PC","0")
+	tribServer.lib.Put("PC", "0")
 	return tribServer, nil
 }
 
 func (ts *tribServer) CreateUser(args *tribrpc.CreateUserArgs, reply *tribrpc.CreateUserReply) error {
 	userID := args.UserID
 	//fmt.Printf("[tribServer] Create user:%s\n",userID)
-	_,err := ts.lib.GetList(userID+"-sub")
+	_, err := ts.lib.GetList(userID + "-sub")
 	if err == nil {
 		//fmt.Printf("[tribServer] user:%d existed\n",userID)
 		reply.Status = tribrpc.Exists
 	} else {
-		key := userID+"-sub"
-		err1 := ts.lib.AppendToList(key,userID)
-		key  = userID+"-trib"
-		err2 := ts.lib.AppendToList(key,"init")
-		if (err1!=nil || err2!=nil){
+		key := userID + "-sub"
+		err1 := ts.lib.AppendToList(key, userID)
+		key = userID + "-trib"
+		err2 := ts.lib.AppendToList(key, "init")
+		if err1 != nil || err2 != nil {
 			fmt.Printf("tmperror\n")
 		}
 		reply.Status = tribrpc.OK
@@ -78,24 +83,24 @@ func (ts *tribServer) AddSubscription(args *tribrpc.SubscriptionArgs, reply *tri
 	userID := args.UserID
 	targetID := args.TargetUserID
 	//fmt.Printf("[tribServer] AddSubscription:%s->%s\n",userID,targetID)
-	userlist,erruser := ts.lib.GetList(userID+"-sub")
-	if erruser !=nil{
+	userlist, erruser := ts.lib.GetList(userID + "-sub")
+	if erruser != nil {
 		reply.Status = tribrpc.NoSuchUser
 		return nil
 	}
-	_,errtarget := ts.lib.GetList(targetID+"-sub")
-	if errtarget !=nil{
+	_, errtarget := ts.lib.GetList(targetID + "-sub")
+	if errtarget != nil {
 		reply.Status = tribrpc.NoSuchTargetUser
 		return nil
 	}
-	for _,user := range(userlist){
-		if user==targetID{
+	for _, user := range userlist {
+		if user == targetID {
 			reply.Status = tribrpc.Exists
 			return nil
 		}
 	}
-	err := ts.lib.AppendToList(userID+"-sub",targetID)
-	if err!=nil {
+	err := ts.lib.AppendToList(userID+"-sub", targetID)
+	if err != nil {
 		return err
 	}
 	reply.Status = tribrpc.OK
@@ -106,19 +111,19 @@ func (ts *tribServer) RemoveSubscription(args *tribrpc.SubscriptionArgs, reply *
 	userID := args.UserID
 	targetID := args.TargetUserID
 	//fmt.Printf("[tribServer] RemoveSubscription:%s->%s\n",userID,targetID)
-	userlist,erruser := ts.lib.GetList(userID+"-sub")
-	if erruser !=nil{
+	userlist, erruser := ts.lib.GetList(userID + "-sub")
+	if erruser != nil {
 		reply.Status = tribrpc.NoSuchUser
 		return nil
 	}
-	_,errtarget := ts.lib.GetList(targetID+"-sub")
-	if errtarget !=nil{
+	_, errtarget := ts.lib.GetList(targetID + "-sub")
+	if errtarget != nil {
 		reply.Status = tribrpc.NoSuchTargetUser
 		return nil
 	}
 	flag := false
-	for _,user := range(userlist){
-		if user==targetID{
+	for _, user := range userlist {
+		if user == targetID {
 			flag = true
 		}
 	}
@@ -126,8 +131,8 @@ func (ts *tribServer) RemoveSubscription(args *tribrpc.SubscriptionArgs, reply *
 		reply.Status = tribrpc.NoSuchTargetUser
 		return nil
 	}
-	err := ts.lib.RemoveFromList(userID+"-sub",targetID)
-	if err!=nil {
+	err := ts.lib.RemoveFromList(userID+"-sub", targetID)
+	if err != nil {
 		return err
 	}
 	reply.Status = tribrpc.OK
@@ -138,19 +143,19 @@ func (ts *tribServer) GetFriends(args *tribrpc.GetFriendsArgs, reply *tribrpc.Ge
 	userID := args.UserID
 	//fmt.Printf("[tribServer] GetFriends:%s\n",userID)
 	var friends []string
-	userlist,erruser := ts.lib.GetList(userID+"-sub")
-	if erruser !=nil{
+	userlist, erruser := ts.lib.GetList(userID + "-sub")
+	if erruser != nil {
 		reply.Status = tribrpc.NoSuchUser
 		return nil
 	}
-	for _,target :=range(userlist){
-		if target==userID {
+	for _, target := range userlist {
+		if target == userID {
 			continue
 		}
-		targetlist,_ := ts.lib.GetList(target+"-sub")
-		for _,tuser := range(targetlist){
-			if tuser==userID {
-				friends = append(friends,target)
+		targetlist, _ := ts.lib.GetList(target + "-sub")
+		for _, tuser := range targetlist {
+			if tuser == userID {
+				friends = append(friends, target)
 			}
 		}
 	}
@@ -161,10 +166,10 @@ func (ts *tribServer) GetFriends(args *tribrpc.GetFriendsArgs, reply *tribrpc.Ge
 
 func (ts *tribServer) PostTribble(args *tribrpc.PostTribbleArgs, reply *tribrpc.PostTribbleReply) error {
 	userID := args.UserID
-	content:= args.Contents
+	content := args.Contents
 	//Step1 : Whether it is a valid user
-	_,err := ts.lib.GetList(userID+"-sub")
-	if err !=nil{
+	_, err := ts.lib.GetList(userID + "-sub")
+	if err != nil {
 		reply.Status = tribrpc.NoSuchUser
 		return nil
 	}
@@ -173,9 +178,9 @@ func (ts *tribServer) PostTribble(args *tribrpc.PostTribbleArgs, reply *tribrpc.
 	//value,_ := ts.lib.Get("PC")
 	//valueInt,_ :=strconv.Atoi(value)
 	//ts.lib.Put("PC",string(valueInt+1))
-	key := "trib-"+strconv.FormatInt(time.Now().UnixNano(),10)
-	ts.lib.AppendToList(userID+"-trib",key)
-	ts.lib.Put(key,content)
+	key := "trib-" + strconv.FormatInt(time.Now().UnixNano(), 10)
+	ts.lib.AppendToList(userID+"-trib", key)
+	ts.lib.Put(key, content)
 	reply.Status = tribrpc.OK
 	reply.PostKey = key
 	//fmt.Printf("[tribServer] PostTribble:%s->%s\n",userID,key)
@@ -184,19 +189,19 @@ func (ts *tribServer) PostTribble(args *tribrpc.PostTribbleArgs, reply *tribrpc.
 
 func (ts *tribServer) DeleteTribble(args *tribrpc.DeleteTribbleArgs, reply *tribrpc.DeleteTribbleReply) error {
 	userID := args.UserID
-	key    := args.PostKey
+	key := args.PostKey
 	//fmt.Printf("[tribServer] Delete Tribble:%s->%s\n",userID,key)
 	//Step1 : Whether it is a valid user
-	triblist,err := ts.lib.GetList(userID+"-trib")
-	if err !=nil{
+	triblist, err := ts.lib.GetList(userID + "-trib")
+	if err != nil {
 		reply.Status = tribrpc.NoSuchUser
 		return nil
 	}
 
 	//Step2 : Whether it is a valid post
 	flag := false
-	for _,trib := range(triblist){
-		if (trib==key){
+	for _, trib := range triblist {
+		if trib == key {
 			flag = true
 			break
 		}
@@ -208,7 +213,7 @@ func (ts *tribServer) DeleteTribble(args *tribrpc.DeleteTribbleArgs, reply *trib
 
 	//Step3: Delete Post
 	ts.lib.Delete(key)
-	ts.lib.RemoveFromList(userID+"-trib",key)
+	ts.lib.RemoveFromList(userID+"-trib", key)
 	reply.Status = tribrpc.OK
 	return nil
 }
@@ -217,8 +222,8 @@ func (ts *tribServer) GetTribbles(args *tribrpc.GetTribblesArgs, reply *tribrpc.
 	userID := args.UserID
 	//fmt.Printf("[tribServer] GetTribble:%s\n",userID)
 	//Step1 : Whether it is a valid user
-	triblist,err := ts.lib.GetList(userID+"-trib")
-	if err !=nil{
+	triblist, err := ts.lib.GetList(userID + "-trib")
+	if err != nil {
 		reply.Status = tribrpc.NoSuchUser
 		return nil
 	}
@@ -228,58 +233,77 @@ func (ts *tribServer) GetTribbles(args *tribrpc.GetTribblesArgs, reply *tribrpc.
 	/*if len(triblist)>100 {
 		triblist = triblist[len(triblist)-101:]
 	}*/
-	for i:=len(triblist)-1;i>0;i-- {
-	    resultlist = append(resultlist,triblist[i])
+	for i := len(triblist) - 1; i > 0; i-- {
+		resultlist = append(resultlist, triblist[i])
 	}
 
 	//Step3 : Return tribble result
 	var tribbles []tribrpc.Tribble
-	length := 0;
-	for i:=0;i<len(resultlist);i++{
-		if length==100 {
-			break;
+	length := 0
+	for i := 0; i < len(resultlist); i++ {
+		if length == 100 {
+			break
 		}
-		contents,err := ts.lib.Get(resultlist[i])
-		if (err!=nil){
+		contents, err := ts.lib.Get(resultlist[i])
+		if err != nil {
 			continue
 		}
-		nano,_     := strconv.ParseInt(resultlist[i][5:], 10, 64)
-		posted     := time.Unix(0, nano)
-		tribble    := tribrpc.Tribble{userID,posted,contents}
-		tribbles    = append(tribbles,tribble)
+		nano, _ := strconv.ParseInt(resultlist[i][5:], 10, 64)
+		posted := time.Unix(0, nano)
+		tribble := tribrpc.Tribble{userID, posted, contents}
+		tribbles = append(tribbles, tribble)
 		//fmt.Printf("Time:%s Content:%s\n",posted.UnixNano,contents)
-		length++;
+		length++
 	}
 	reply.Status = tribrpc.OK
 	reply.Tribbles = tribbles
 	return nil
 }
 
+type tribSlice [][2]string
+
+func (c tribSlice) Len() int {
+	return len(c)
+}
+func (c tribSlice) Swap(i, j int) {
+	var ele [2]string
+	ele[0] = c[i][0]
+	ele[1] = c[i][1]
+	c[i][0] = c[j][0]
+	c[i][1] = c[j][1]
+	c[j][0] = ele[0]
+	c[j][1] = ele[1]
+}
+
+func (c tribSlice) Less(i, j int) bool {
+	return c[i][0] > c[j][0]
+}
+
 func (ts *tribServer) GetTribblesBySubscription(args *tribrpc.GetTribblesArgs, reply *tribrpc.GetTribblesReply) error {
 	userID := args.UserID
 	//fmt.Printf("[tribServer] GetTribble:%s\n",userID)
 	//Step1 : Whether it is a valid user
-	userlist,err := ts.lib.GetList(userID+"-sub")
-	if err !=nil{
+	userlist, err := ts.lib.GetList(userID + "-sub")
+	if err != nil {
 		reply.Status = tribrpc.NoSuchUser
 		return nil
 	}
 
 	//Step2 : Select all tribs from targets
-	var triblist [][2]string
-	for _,user := range(userlist){
-		if user==userID {
+	var triblist tribSlice
+	for _, user := range userlist {
+		if user == userID {
 			continue
 		}
-		tribunits,_ := ts.lib.GetList(user+"-trib")
-		for _,trib := range(tribunits){
-			if trib==string("init") {
+		tribunits, _ := ts.lib.GetList(user + "-trib")
+		for _, trib := range tribunits {
+			if trib == string("init") {
 				continue
 			}
 			var ele [2]string
 			ele[0] = trib
 			ele[1] = user
-			triblist = append(triblist,ele)
+			triblist = append(triblist, ele)
 			//tribvalue,_ := strconv.ParseInt(trib[5:], 10, 64)
 			//triblist = append(triblist,float64(tribvalue))
 		}
@@ -288,26 +312,27 @@ func (ts *tribServer) GetTribblesBySubscription(args *tribrpc.GetTribblesArgs, r
 	//Step3 : Sort the tribble list
 	//sort.Sort(sort.Reverse(sort.Float64Slice(triblist)))
 	//sort.Sort(sort.Reverse(sort.Slice(triblist,func(i, j int) bool { return triblist[i][0] < triblist[j][0] })))
-	sort.Slice(triblist,func(i, j int) bool { return triblist[i][0] > triblist[j][0] })
+	//sort.Slice(triblist,func(i, j int) bool { return triblist[i][0] > triblist[j][0] })
+	sort.Sort(triblist)
 	var tribbles []tribrpc.Tribble
-	length := 0;
-	for i:=0;i<len(triblist);i++{
+	length := 0
+	for i := 0; i < len(triblist); i++ {
 		//trib:= strconv.FormatInt(int64(triblist[i]),10)
 		trib := triblist[i][0]
-		if length==100 {
-			break;
+		if length == 100 {
+			break
 		}
-		contents,err := ts.lib.Get(trib)
-		if (err!=nil){
+		contents, err := ts.lib.Get(trib)
+		if err != nil {
 			//fmt.Println("Key not Found")
 			continue
 		}
-		nano,_     := strconv.ParseInt(trib[5:], 10, 64)
-		posted     := time.Unix(0, nano)
-		tribble    := tribrpc.Tribble{triblist[i][1],posted,contents}
-		tribbles    = append(tribbles,tribble)
+		nano, _ := strconv.ParseInt(trib[5:], 10, 64)
+		posted := time.Unix(0, nano)
+		tribble := tribrpc.Tribble{triblist[i][1], posted, contents}
+		tribbles = append(tribbles, tribble)
 		//fmt.Printf("Add tribble:%s,%s\n",tribble.UserID,posted.String())
-		length++;
+		length++
 	}
 	reply.Status = tribrpc.OK
 	reply.Tribbles = tribbles
